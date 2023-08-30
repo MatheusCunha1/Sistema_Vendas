80fea6e00059b14f772f3552e0c4aff3b8c204c8
matheus alves <matheuaalves@outlook.com>
2023-08-30 11:52:56 -0300

    Menu Finalizado

--- GF_1/GF_1/Program.cs
+++ GF_1/GF_1/Program.cs
@@ -8,56 +8,69 @@ namespace GF_1
         static void Main(string[] args)
         {
             List<Cliente> listaClientes = new List<Cliente>();
-        
 
             int op;
-            Console.WriteLine("|======|Sistema MAPA|=======|");
-            Console.WriteLine("| [1] Adicionar Cliente     |");
-            Console.WriteLine("| [2] Mostrar Cliente       |");
-            Console.WriteLine("| [3] Adicionar Categoria   |");
-            Console.WriteLine("| [4] Realizar Venda        |");
-            Console.WriteLine("| [5] Sair                  |");
-            Console.WriteLine("|===========================|");
-            Console.Write("Digite a opção desejada: ");
-            if (int.TryParse(Console.ReadLine(), out op))
+
+            do
             {
-                switch (op)
+                Console.Clear();
+                Console.WriteLine("|======|Sistema MAPA|=======|");
+                Console.WriteLine("| [1] Adicionar Cliente     |");
+                Console.WriteLine("| [2] Mostrar Cliente       |");
+                Console.WriteLine("| [3] Adicionar Categoria   |");
+                Console.WriteLine("| [4] Realizar Venda        |");
+                Console.WriteLine("| [5] Sair                  |");
+                Console.WriteLine("|===========================|");
+                Console.Write("Digite a opção desejada: ");
+
+                if (int.TryParse(Console.ReadLine(), out op))
                 {
-                    case 1:
-                        Console.Clear(); 
-                        Console.WriteLine("Opção 1 selecionada: Adicionar Cliente");
+                    switch (op)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Opção 1 selecionada: Adicionar Cliente");
+                            Console.ResetColor();
 
-                        Console.Write("Nome: ");
-                        string nome = Console.ReadLine();
+                            do
+                            {
+                                Console.Write("Nome: ");
+                                string nome = Console.ReadLine();
 
-                        Console.Write("Sobrenome: ");
-                        string sobrenome = Console.ReadLine();
+                                Console.Write("Sobrenome: ");
+                                string sobrenome = Console.ReadLine();
 
-                        Console.Write("Endereço: ");
-                        string endereco = Console.ReadLine();
+                                Console.Write("Endereço: ");
+                                string endereco = Console.ReadLine();
 
-                        Console.Write("Número de Telefone: ");
-                        string numeroTelefone = Console.ReadLine();
+                                Console.Write("Número de Telefone: ");
+                                string numeroTelefone = Console.ReadLine();
 
-                        Cliente.AdicionarCliente(listaClientes, nome, sobrenome, endereco, numeroTelefone);
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Cliente.AdicionarCliente(listaClientes, nome, sobrenome, endereco, numeroTelefone);
+                                Console.ResetColor();
 
-                        Console.WriteLine("Cliente adicionado com sucesso!");
-                        Console.ReadKey();
-                        break;
+                                Console.WriteLine("Deseja adicionar mais um cliente? (S/N)");
+                            } while (Console.ReadLine().Trim().ToUpper() == "S");
+                            break;
 
-                    default:
-                        Console.Clear(); // Limpa a tela
-                        Console.WriteLine("Opção inválida");
-                        Console.ReadKey();
-                        break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Opção inválida");
+                            break;
+                    }
                 }
-            }
-            else
-            {
-                Console.Clear(); // Limpa a tela
-                Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
-                Console.ReadKey();
-            }
+                else
+                {
+                    Console.Clear(); // Limpa a tela
+                    Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+                }
+
+            } while (op != 5); // Continue o loop enquanto a opção não for "Sair"
+
+            Console.WriteLine("Saindo do programa.");
+            Console.ReadKey();
         }
     }
 }
