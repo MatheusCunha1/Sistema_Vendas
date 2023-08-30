8afb98f26eefc6465726f80f73d798957fc5c2a2
Eduarda Bassani <bassanieduardaa@gmail.com>
2023-08-30 11:30:05 -0300

    class venda

--- GF_1/GF_1/Produto.cs
+++ GF_1/GF_1/Produto.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace GF_1
 {
-    internal class Produto
+
+    class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    
     }
+
 }
