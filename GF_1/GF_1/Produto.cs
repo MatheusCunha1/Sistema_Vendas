7d284f25eb276c111c9998ae49044ac28d31a1c5
Eduarda Bassani <bassanieduardaa@gmail.com>
2023-08-30 11:55:08 -0300

    add

--- GF_1/GF_1/Produto.cs
+++ GF_1/GF_1/Produto.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GF_1
 {
 
-    class Produto
+    public class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
-    
+
+        public Produto(int id,string nome, string descricao , decimal preco)
+        {
+
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+        }
+
+        public static bool AdicionarCliente(List<Produto> listaProduto, int id, string nome, string descricao, decimal preco)
+        {
+            try
+            {
+                Produto novoProduto = new Produto (id,  nome, descricao, preco);
+                listaProduto.Add(novoProduto);
+                Console.WriteLine("Produto adicionado com sucesso.");
+                return true;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Erro ao adicionar Produto: " + e.Message);
+                return false;
+            }
+        }
+
     }
 
 }
