3b441d21f86c8550e5c2abada165cbe49cc7fa1c
matheus alves <matheuaalves@outlook.com>
2023-08-29 21:19:56 -0300

    Menu do sistema

--- GF_1/GF_1/Program.cs
+++ GF_1/GF_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,45 @@ namespace GF_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Paulo");
+            bool clienteAdicionado1 = Cliente.AdicionarCliente(clientes, "João", "Silva", "Rua A, 123", "(11) 1234-5678");
+            int op;
+            Console.WriteLine("|======|Sistema MAPA|=======|");
+            Console.WriteLine("| [1] Adicionar Cliente     |");
+            Console.WriteLine("| [2] Adicionar Produto     |");
+            Console.WriteLine("| [3] Adicionar Categoria   |");
+            Console.WriteLine("| [4] Realizar Venda        |");
+            Console.WriteLine("|===========================|");
+            Console.Write("Digite a opção desejada: ");
+            if (int.TryParse(Console.ReadLine(), out op))
+            {
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Opção 1 selecionada: Adicionar Cliente");
+                        List<Cliente> clientes = new List<Cliente>();
+                        if (clienteAdicionado1)
+                        {
+                            Console.WriteLine("Cliente 1 foi adicionado com sucesso.");
+                        }
+
+                        if (clienteAdicionado2)
+                        {
+                            Console.WriteLine("Cliente 2 foi adicionado com sucesso.");
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+            }
         }
     }
+
+}
 }
